0db3f5d9f960325d7c36da9e094cbd737f04d43a
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-04 23:12:15 -0500

    update

--- .github/workflows/toc.py
+++ .github/workflows/toc.py
@@ -1,18 +1,42 @@
 import os
-import glob
 
-os.system('rm -rf README.md')
+def is_markdown(path):
+    # file is .md
+    if os.path.isfile(path) and path.endswith('.md') and path.split('/')[-1] != 'README.md':
+        return True
+    
+    # one of subdirectory is .md
+    elif os.path.isdir(path):
+        return any([is_markdown(os.path.join(path, node)) for node in os.listdir(path)])
 
-readme_contents = "# Table of Contents\n\n"
 
-for foldername, subfolders, filenames in os.walk('.'):
-    for filename in filenames:
-        if filename.endswith('.md') and not filename == 'README.md':
-            path = os.path.join(foldername, filename)
-            path = os.path.normpath(path)
-            link = path.replace('\\', '/')
-            name = os.path.splitext(filename)[0]
-            readme_contents += f"- [{name}]({link})\n"
+def gen_toc(startdir, indent=0):
+    toc = ''
+    
+    for node in os.listdir(startdir):
+        path = os.path.join(startdir, node)
 
-with open('README.md', 'w') as f:
-    f.write(readme_contents)
+        if not is_markdown(path):
+            continue
+
+        if os.path.isfile(path):
+            toc += '\t'*indent + f"- [{node.split('.')[0]}](<{path}>)\n"
+
+        elif os.path.isdir(path):
+            toc += '\t'*indent + '- ' + node + '\n'
+            toc += gen_toc(path, indent + 1)
+    
+    return toc
+
+
+if __name__ == '__main__':
+    os.system('rm -rf README.md && touch README.md')
+    
+    toc = gen_toc('.')
+    with open('README.md', 'w') as f:
+        f.write('_last posts:_\n\n')
+        f.write(toc)
+        f.write('\n\n')
+        f.write('---')
+        f.write('\n\n')
+        f.write('subscribe via the github watch list: https://github.com/sueszli/blog/subscription')
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
+_last posts:_
+
+- phd stats
+	- [phd stats](<./phd stats/phd stats.md>)
+- ds tanenbaum
+	- [distributed systems](<./ds tanenbaum/distributed systems.md>)
+- [barrier pattern](<./barrier pattern.md>)
+- paxos simple
+	- [paxos simple](<./paxos simple/paxos simple.md>)
+- ffs - fast file system
+	- [ffs](<./ffs - fast file system/ffs.md>)
+- [choosing a language](<./choosing a language.md>)
+- [choosing macos](<./choosing macos.md>)
+- farm - fast remote memory
+	- [farm](<./farm - fast remote memory/farm.md>)
+- aurora - single level storage
+	- [aurora](<./aurora - single level storage/aurora.md>)
+- lucene is all you need
+	- [introduction to ir](<./lucene is all you need/introduction to ir.md>)
+	- [lucene is all you need](<./lucene is all you need/lucene is all you need.md>)
+	- [anserini gets dense retrieval](<./lucene is all you need/anserini gets dense retrieval.md>)
+- raft
+	- [raft](<./raft/raft.md>)
+- [systems is ambiguous](<./systems is ambiguous.md>)
+- castor - record replay system
+	- [castor](<./castor - record replay system/castor.md>)
+- [choosing a note taking app](<./choosing a note taking app.md>)
+- xv6 os
+	- [xv6 os](<./xv6 os/xv6 os.md>)
+
+
+---
+
 subscribe via the github watch list: https://github.com/sueszli/blog/subscription
