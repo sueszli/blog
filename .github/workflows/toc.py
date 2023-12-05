fd79d535b3f6556055b3ad3f0ed55c4353fc1502
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-04 23:24:24 -0500

    update

--- .github/workflows/toc.py
+++ .github/workflows/toc.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     toc = gen_toc('.')
     
     with open('README.md', 'w') as f:
-        f.write('## sueszli\'s blog')
+        f.write('_posts:_')
         f.write('\n\n')
         f.write(toc)
         f.write('\n\n')
