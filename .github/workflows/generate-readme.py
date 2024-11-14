0654b809bfab1a90e1b1c07a9cbff0b0beb08755
Yahya Jabary <61852663+sueszli@users.noreply.github.com>
2024-11-14 22:11:15 +0100

    Update generate-readme.py

--- .github/workflows/generate-readme.py
+++ .github/workflows/generate-readme.py
@@ -93,8 +93,6 @@ def main():
         f.write("\n\n")
         f.write("to stay up to date with new posts subscribe via github: [https://github.com/sueszli/blog/subscription](https://github.com/sueszli/blog/subscription)")
         f.write("\n\n")
-        f.write("<br>")
-        f.write("\n\n")
         f.write("_file tree:_")
         f.write("\n\n")
         f.write(toc)
